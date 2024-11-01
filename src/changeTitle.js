9e706d7fea4c47a81a151fe88c6d8f2c5d315fc4
asa9874 <pjkpjk1129@gmail.com>
2024-11-02 00:35:33 +0900

    Feat: Change Title

---
+++ src/changeTitle.js
@@ -0,0 +1,13 @@
+import $ from 'jquery'
+function getRandomString(length) {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+setInterval(()=>{
+    $('title').text(getRandomString(20));
+}, 1000);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-base: '/useful-web/', // GitHub Pages의 서브 디렉토리 경로
+base: '/dangerous-web/', // GitHub Pages의 서브 디렉토리 경로
 });
